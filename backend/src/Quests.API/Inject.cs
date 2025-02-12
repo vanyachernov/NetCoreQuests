f27d51f9f5ee1e2e0f8e36ee2cb6258fe8c44df7
Vanya Chernov <vanyachernov2001@gmail.com>
2025-02-12 07:20:47 +0200

    Add images and docker-compose file

--- backend/src/Quests.API/Inject.cs
+++ backend/src/Quests.API/Inject.cs
@@ -8,27 +8,27 @@ public static class Inject
     public static IServiceCollection AddApi(this IServiceCollection services)
     {
         DotNetEnv.Env.Load();
-        
+
         services.AddDbContext<QuestDbContext>(options =>
         {
             var dbServer = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost";
-            var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
+            var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "5433";
             var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "QuestsDb";
             var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "user";
             var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "password";
 
-            var connectionString = $"Host={dbServer};" +
-                                   $"Port={dbPort};" +
-                                   $"Database={dbName};" +
-                                   $"Username={dbUser};" +
-                                   $"Password={dbPassword};" +
-                                   "Trust Server Certificate=true;";
+            var connectionString =
+                $"Host={dbServer};"
+                + $"Port={dbPort};"
+                + $"Database={dbName};"
+                + $"Username={dbUser};"
+                + $"Password={dbPassword};"
+                + "SSL Mode=Require;"
+                + "Trust Server Certificate=true;";
 
-            options
-                .UseNpgsql(connectionString)
-                .UseSnakeCaseNamingConvention();
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
         });
-        
+
         return services;
     }
 }
